7c09449582f869a55d0adfe3ea30ac4e33a267c3
Kyle Hayes <khayes@hey.com>
2025-02-28 07:49:47 -0800

    fix: removed old GA code. update README

--- main.py
+++ main.py
@@ -21,42 +21,6 @@ def handle_exception(e):
     logger.exception("unhandled exception: %s", str(e))
     return "Server Error", 500
 
-GA_TRACKING_ID = os.environ.get('GA_TRACKING_ID')
-app.config['FLASKS3_BUCKET_NAME'] = os.environ.get('FPOIMG_AWS_BUCKET')
-app.config['FLASKS3_FILEPATH_HEADERS'] = {
-    r'.css$': {
-        'Content-Type': 'text/css',
-    },
-    r'.js$': {
-        'Content-Type': 'text/javascript',
-    }
-}
-# s3 = Flask(app)
-
-
-def track_event(category, action, label=None, value=0, referrer=555):
-    data = {
-        'v': '1',  # API Version.
-        'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
-        # Anonymous Client Identifier. Ideally, this should be a UUID that
-        # is associated with particular user, device, or browser instance.
-        'cid': referrer,
-        't': 'event',  # Event hit type.
-        'ec': category,  # Event category.
-        'ea': action,  # Event action.
-        'el': label,  # Event label.
-        'ev': value,  # Event value, must be an integer
-    }
-
-    response = requests.post(
-        'https://www.google-analytics.com/collect', data=data)
-
-    # If the request fails, this will raise a RequestException. Depending
-    # on your application's needs, this may be a non-error and can be caught
-    # by the caller.
-    # print(data)
-    response.raise_for_status()
-
 @app.route("/")
 def home():
   logger.info("Home page")
@@ -224,36 +188,6 @@ def generate(width, height, caption="", bg_color=(100,100,100), text_color=(200,
 
   del draw # I'm done drawing so I don't need this anymore
   
-  ts = time.time()
-  timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-  logData = {
-    "timestamp": timestamp,
-    "width": width,
-    "height": height,
-    "caption": caption,
-    "bg_color": bg_color,
-    "text_color": text_color,
-    "referrer": request.referrer,
-    "user_agent": request.user_agent
-  }
-
-  event_data = {
-    "width": width,
-    "height": height,
-    "caption": caption,
-    "bg_color": bg_color,
-    "text_color": text_color,
-    "referrer": request.referrer,
-  }
-
-  track_event(
-    'Image',
-    'generate',
-    label=",".join(['%s:%s'% (v,event_data[v]) for v in event_data]),
-    referrer=request.referrer
-  )
-
   return serve_pil_image(im)
 
 
