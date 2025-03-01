cb9b990292394ee0f8092c54903619152b39f9a6
Kyle Hayes <khayes@hey.com>
2025-02-28 18:43:37 -0800

    fix: better 404 handling

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ import datetime
 import io
 import textwrap
 import requests
+from werkzeug.exceptions import HTTPException
+import json
 
 app = Flask(__name__)
 
 # configure logging
 logging.basicConfig(level=logging.INFO)
 logger = app.logger
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 @app.errorhandler(Exception)
 def handle_exception(e):
